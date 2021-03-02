79c06e94487ca4debe1ebcd0f33032b4c02a77e1
azahmouni <abdelkrim.zahmouni@gmail.com>
2021-03-02 22:12:08 +0000

    testPrintFromOneToNine() is OK.

--- src/main/java/fr/sg/foobarqix/FooBarQixPrinterDefault.java
+++ src/main/java/fr/sg/foobarqix/FooBarQixPrinterDefault.java
@@ -16,7 +16,7 @@ public class FooBarQixPrinterDefault implements FooBarQixPrinter {
 		String strValue			= "";
 		boolean printTheNumber = true;
 		
-		for(int i = 1; i <= 7; i++) {
+		for(int i = 1; i <= 9; i++) {
 			
 			printTheNumber = true;
 			divisibleBy3 = i % 3 == 0;
