bfabc07709c17409eb52d51cd1c5550edf0d6828
azahmouni <abdelkrim.zahmouni@gmail.com>
2021-03-02 22:25:04 +0000

    Creating a new test method: testPrintFromOneToTwentyNine() to cover printing 1 to 29. The test is KO, maybe we should enhance the FooBarQixPrinterDefault.print() method more.

--- src/main/java/fr/sg/foobarqix/FooBarQixPrinterDefault.java
+++ src/main/java/fr/sg/foobarqix/FooBarQixPrinterDefault.java
@@ -16,7 +16,7 @@ public class FooBarQixPrinterDefault implements FooBarQixPrinter {
 		String strValue			= "";
 		boolean printTheNumber = true;
 		
-		for(int i = 1; i <= 29; i++) {
+		for(int i = 1; i <= 15; i++) {
 			
 			printTheNumber = true;
 			divisibleBy3 = i % 3 == 0;
