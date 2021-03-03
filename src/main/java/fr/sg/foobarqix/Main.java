8c3a00cc148e6eda4ddcc828fed94adf7e6cde69
azahmouni <abdelkrim.zahmouni@gmail.com>
2021-03-03 09:10:41 +0000

    Ajout de la méthode main()

---
+++ src/main/java/fr/sg/foobarqix/Main.java
@@ -0,0 +1,10 @@
+package fr.sg.foobarqix;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		FooBarQixPrinter printer = new FooBarQixPrinterDefault();
+		printer.print();
+	}
+}
